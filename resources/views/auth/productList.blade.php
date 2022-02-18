d0943c34104a2f6e28633bf786705586dae54315
keikei046 <kei666666@gmail.com>
2022-02-18 13:49:01 +0800

    update product list

--- resources/views/auth/productList.blade.php
+++ resources/views/auth/productList.blade.php
@@ -3,20 +3,32 @@
 @section('content')
 
 
+<!-- <?php
+    // $productList = DB::select('select * from product');
+?>
+    <div class="productList">
     <?php
-        $productList = DB::select('select * from product');
+        // foreach ($productList as $product){
+        //     echo '<div class="product">'.$product->productID.'<br>'
+        //     .$product->productName.'</div>';
+        //}
     ?>
-        <div class="productList">
-        <?php
-            foreach ($productList as $product){
-                echo '<div class="product">'.$product->productID.'<br>'
-                .$product->productName.'</div>';
-            }
-        ?>
-        </div>
+    </div> -->
 
 
+<?php
+    $dbConnection = mysqli_connect("localhost", "root", "", "isi");
+    $productList = mysqli_query($dbConnection, "SELECT * FROM `product");
 
+    while ($product = mysqli_fetch_assoc($productList)){
+        echo
+        '<div class="product">'
+        .$product['productID'].'<br>'
+        .$product['productName'].
+        '</div><br>';
+
+    }
+?>
 
 
 @endsection
