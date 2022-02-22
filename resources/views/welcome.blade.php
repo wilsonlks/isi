2ad393fde352bea05150f2e1b49dcf658b886302
wilsonlks <81558095+wilsonlks@users.noreply.github.com>
2022-02-22 17:41:49 +0800

    Revert "change product link location"
    This reverts commit 33e3c95815c83ce140310bc49d8546aec44f8ff8.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
 
-        <!-- <a href="{{ url('/productList') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Product</a> -->
+        <a href="{{ url('/productList') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Product</a>
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
@@ -126,26 +126,19 @@
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 sm:text-left">
                         <div class="flex items-center">
-                            <div class="flex">
-                                <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
-                                    <path d=""></path>
-                                </svg>
+                            <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
+                                <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                            </svg>
+
                                         <br>
                                         <br>
-                                <a href="{{ url('/productList') }}" class="ml-1 underline">
-                                    Product
-                                </a>
-                            </div>
-                            <div class="flex">
-                                <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="-mt-px w-5 h-5 text-gray-400">
-                                    <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                                </svg>
-                                        <br>
-                                        <br>
-                                <a href="http://localhost:8000/login" class="ml-1 underline">
-                                    Shop
-                                </a>
-                            </div>
+                            <a href="http://localhost:8000/login" class="ml-1 underline">
+                                Shop
+                            </a>
+
+
+
+
                         </div>
                     </div>
 
