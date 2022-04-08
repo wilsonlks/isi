548fd37af199e19ed84cb90ad14238e4c0eaabab
keikei046 <kei666666@gmail.com>
2022-04-08 13:31:52 +0800

    change error message for login

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
+    'failed' => 'The email address or password is incorrect. Please try again.',
+    //'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
